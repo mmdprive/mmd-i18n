2f013891d2f4de46b77f30a1cc0a9b766edc20e9
MMD <malemodel.bkk@gmail.com>
2025-12-31 19:12:41 +0700

    Update i18n.dict.js

--- assets/i18n/i18n.dict.js
+++ assets/i18n/i18n.dict.js
@@ -1,11 +1,19 @@
 window.I18N_DICT = window.I18N_DICT || {};
+
 Object.assign(window.I18N_DICT, {
   th: Object.assign({}, window.I18N_DICT.th || {}, {
-    "lang.en": "English",
-    "benefits_lite.title": "สิทธิ์สมาชิก (สรุป)"
+    "lang.th": "ไทย"
   }),
+
   en: Object.assign({}, window.I18N_DICT.en || {}, {
-    "lang.en": "English",
-    "benefits_lite.title": "Benefits"
+    "lang.en": "English"
+  }),
+
+  zh: Object.assign({}, window.I18N_DICT.zh || {}, {
+    "lang.zh": "中文"
+  }),
+
+  jp: Object.assign({}, window.I18N_DICT.jp || {}, {
+    "lang.jp": "日本語"
   })
 });
